f2b96ebc9246fb23a095a294b1998aa5a615acb2
Dylan Manley <C00286789@setu.ie>
2025-11-12 13:21:52 +0000

    grid set up

---
+++ SFML Template 3.0.01(working)1/SFML Template 3.0.01(working)1/Game.cpp
@@ -0,0 +1,190 @@
+/// <summary>
+/// author Dylan Manley
+/// you need to change the above line or lose marks
+/// </summary>
+
+
+#include "Game.h"
+#include <iostream>
+
+/// <summary>
+/// default constructor
+/// setup the window properties
+/// load and setup the texts
+/// load and setup the images
+/// load and setup the sounds
+/// </summary>
+Game::Game() :
+	m_window{ sf::VideoMode{ sf::Vector2u{500, 600}, 32U }, "The Fourth Protocol" },
+	m_DELETEexitGame{false} //when true game will exit
+{
+	setupTexts(); // load font 
+	setupSprites(); // load texture
+	setupAudio(); // load sounds
+	setupGrid();
+}
+
+/// <summary>
+/// default destructor we didn't dynamically allocate anything
+/// so we don't need to free it, but mthod needs to be here
+/// </summary>
+Game::~Game()
+{
+}
+
+
+/// <summary>
+/// main game loop
+/// update 60 times per second,
+/// process update as often as possible and at least 60 times per second
+/// draw as often as possible but only updates are on time
+/// if updates run slow then don't render frames
+/// </summary>
+void Game::run()
+{	
+	sf::Clock clock;
+	sf::Time timeSinceLastUpdate = sf::Time::Zero;
+	const float fps{ 60.0f };
+	sf::Time timePerFrame = sf::seconds(1.0f / fps); // 60 fps
+	while (m_window.isOpen())
+	{
+		processEvents(); // as many as possible
+		timeSinceLastUpdate += clock.restart();
+		while (timeSinceLastUpdate > timePerFrame)
+		{
+			timeSinceLastUpdate -= timePerFrame;
+			processEvents(); // at least 60 fps
+			update(timePerFrame); //60 fps
+		}
+		render(); // as many as possible
+	}
+}
+
+/// </summary>
+void Game::processEvents()
+{
+	
+	while (const std::optional newEvent = m_window.pollEvent())
+	{
+		if ( newEvent->is<sf::Event::Closed>()) // close window message 
+		{
+			m_DELETEexitGame = true;
+		}
+		if (newEvent->is<sf::Event::KeyPressed>()) //user pressed a key
+		{
+			processKeys(newEvent);
+		}
+
+	}
+}
+
+
+/// <summary>
+/// deal with key presses from the user
+/// </summary>
+/// <param name="t_event">key press event</param>
+void Game::processKeys(const std::optional<sf::Event> t_event)
+{
+	const sf::Event::KeyPressed *newKeypress = t_event->getIf<sf::Event::KeyPressed>();
+	if (sf::Keyboard::Key::Escape == newKeypress->code)
+	{
+		m_DELETEexitGame = true; 
+	}
+}
+
+/// <summary>
+/// Check if any keys are currently pressed
+/// </summary>
+void Game::checkKeyboardState()
+{
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Escape))
+	{
+		m_DELETEexitGame = true; 
+	}
+}
+
+/// <summary>
+/// Update the game world
+/// </summary>
+/// <param name="t_deltaTime">time interval per frame</param>
+void Game::update(sf::Time t_deltaTime)
+{
+	checkKeyboardState();
+	if (m_DELETEexitGame)
+	{
+		m_window.close();
+	}
+}
+
+/// <summary>
+/// draw the frame and then switch buffers
+/// </summary>
+void Game::render()
+{
+	m_window.clear(sf::Color::White);
+
+	for (int i = 0; i < GRID_SIZE; i++)
+	{
+		m_window.draw(grid[i]);
+	}
+
+	m_window.display();
+}
+
+/// <summary>
+/// load the font and setup the text message for screen
+/// </summary>
+void Game::setupTexts()
+{
+	if (!m_jerseyFont.openFromFile("ASSETS\\FONTS\\Jersey20-Regular.ttf"))
+	{
+		std::cout << "problem loading arial black font" << std::endl;
+	}
+	m_DELETEwelcomeMessage.setFont(m_jerseyFont);
+	m_DELETEwelcomeMessage.setString("SFML Game");
+	m_DELETEwelcomeMessage.setPosition(sf::Vector2f{ 205.0f, 240.0f });
+	m_DELETEwelcomeMessage.setCharacterSize(96U);
+	m_DELETEwelcomeMessage.setOutlineColor(sf::Color::Black);
+	m_DELETEwelcomeMessage.setFillColor(sf::Color::Red);
+	m_DELETEwelcomeMessage.setOutlineThickness(2.0f);
+
+}
+
+
+/// <summary>
+/// load the texture and setup the sprite for the logo
+/// </summary>
+void Game::setupSprites()
+{
+		
+}
+
+/// <summary>
+/// load sound file and assign buffers
+/// </summary>
+void Game::setupAudio()
+{
+
+}
+
+void Game::setupGrid()
+{
+	for (int i = 0; i < GRID_SIZE; i++)
+	{
+		grid[i].setPosition(sf::Vector2f{ xPos, yPos });
+		grid[i].setSize(sf::Vector2f{ 100, 100 });
+		grid[i].setFillColor(sf::Color::White);
+		grid[i].setOutlineColor(sf::Color::Black);
+		grid[i].setOutlineThickness(5);
+
+		if (xPos >= 500)
+		{
+			yPos += 100;
+			xPos = 2;
+		}
+		else
+		{
+			xPos += 100;
+		}
+	}
+}
